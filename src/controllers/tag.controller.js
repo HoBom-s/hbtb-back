e5c7ec1e65fe61d8190d1ccea10c2d834c5fa2bc
foxmon <jjockrod@naver.com>
2023-05-08 21:42:02 +0900

    tag controller

--- src/controllers/tag.controller.js
+++ src/controllers/tag.controller.js
@@ -1,17 +1,42 @@
 import tagService from "../services/tag.service";
+import APIErrorHandler from "../helpers/error.helper";
 
-const tagController = {
-  createTagRequest: async function (req, res) {
+/**
+ * Tag controller
+ */
+const tagController = {};
+
+tagController.getAllTagRequest = async function (req, res) {
+  try {
+    const tags = await tagService.getAllTagRequest();
+    res.status(200).send(tags);
+  } catch (error) {
+    const apiError = new APIErrorHandler(
+      `Get all tag request controller fail with ${error.message}`,
+      error.status
+    );
+    const { status, msg } = apiError;
+    res.status(status).send({
+      message: msg,
+    });
+  }
+};
+
+tagController.createTagRequest = async function (req, res) {
+  try {
     const { title } = req.body;
     const createdTag = await tagService.createTagRequest(title);
-    if (createdTag?.error) {
-      const { status, msg } = createdTag.error;
-      return res.status(status).send({
-        message: msg,
-      });
-    }
     res.status(200).send(createdTag);
-  },
+  } catch (error) {
+    const apiError = new APIErrorHandler(
+      `Create tag request controller fail with ${error.message}`,
+      error.status
+    );
+    const { status, msg } = apiError;
+    res.status(status).send({
+      message: msg,
+    });
+  }
 };
 
 Object.freeze(tagController);
