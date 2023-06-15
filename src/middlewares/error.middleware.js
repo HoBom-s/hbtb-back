932294c0353f6a3794f9a5cac40d11879acf0f9d
foxmon <jjockrod@naver.com>
2023-06-15 22:56:18 +0900

    user & error middleware

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -5,20 +5,22 @@
  *
  * @param {Error} error
  */
+
+// eslint-disable-next-line no-unused-vars
 function errorMiddleware(error, req, res, next) {
   const bodyData = req.body;
   const { status, message } = error;
   if (status >= 400 && status < 500) {
     // Request error
     const errorStatus = status || 400;
-    res.status(errorStatus).send({
+    return res.status(errorStatus).send({
       data: bodyData,
       message: `${errorStatus}: ${message}`,
     });
   } else if (status >= 500) {
     // Server internal error
     const errorStatus = status || 500;
-    res.status(errorStatus).send({
+    return res.status(errorStatus).send({
       data: bodyData,
       message: `${errorStatus}: ${message}`,
     });
